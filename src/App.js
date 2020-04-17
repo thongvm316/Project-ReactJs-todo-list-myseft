48f42ce5108117d0641551dbdfe7ada63e1cb2bc
Minh Thong <vominhthong1603@gmail.com>
2020-04-17 12:14:42 +0700

    Restore

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import TodoList from './components/TodoList';;
 class App extends Component {
   render() {
     return (
-      // <div>
-      //   <div className="container">
-      //     <div className="row">
-      //       <div className="col-10 mx-auto col-md-8 mt-4">
-      //         <h3 className="text-capitalize text-center">Todo Input</h3>
-      //         <TodoInput />
-      //       </div>
-      //     </div>
-      //  </div>
-      // </div>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-4">
+              <h3 className="text-capitalize text-center">Todo Input</h3>
+              <TodoInput />
+            </div>
+          </div>
+       </div>
+      </div>
     );
   }
 }
